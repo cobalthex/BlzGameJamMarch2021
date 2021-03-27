0872a1708f2ef53ef152809adcd778fdbcb7e0cf
Aaron Doukas <amyleeslave@gmail.com>
2021-03-27 14:55:15 -0700

    Monster can now chase player

--- unity/Assets/Scripts/Chase.cs
+++ unity/Assets/Scripts/Chase.cs
@@ -4,10 +4,26 @@ using UnityEngine.AI;
 public class Chase : MonoBehaviour
 {
     public Transform target;
+    public float updateSeconds;
+
+    private float updateCountdown;
+    private NavMeshAgent agent;
+
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         agent.destination = target.position;
+        updateCountdown = updateSeconds;
+    }
+
+    void FixedUpdate()
+    {
+        updateCountdown -= Time.fixedDeltaTime;
+        if (updateCountdown <= 0)
+        {
+            agent.destination = target.position;
+            updateCountdown = updateSeconds;
+        }
     }
 }
