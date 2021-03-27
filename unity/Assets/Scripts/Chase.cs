d77fbed164a080625f6d2fa21e9d8460f5cd9470
Aaron Doukas <amyleeslave@gmail.com>
2021-03-27 15:11:23 -0700

    Added code that can detect if player is no longer reachable; we can use this to trigger warps of the monster as needed

--- unity/Assets/Scripts/Chase.cs
+++ unity/Assets/Scripts/Chase.cs
@@ -8,6 +8,7 @@ public class Chase : MonoBehaviour
 
     private float updateCountdown;
     private NavMeshAgent agent;
+    private bool isPartial;
 
 
     void Start()
@@ -15,10 +16,23 @@ public class Chase : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.destination = target.position;
         updateCountdown = updateSeconds;
+        isPartial = false;
     }
 
     void FixedUpdate()
     {
+        if (agent.pathStatus == NavMeshPathStatus.PathPartial && !isPartial)
+        {
+            Debug.Log("Can't reach dest anymore");
+            isPartial = true;
+        }
+        else if (agent.pathStatus == NavMeshPathStatus.PathComplete && isPartial)
+        {
+            Debug.Log("Can now reach dest");
+            isPartial = false;
+        }
+        
+
         updateCountdown -= Time.fixedDeltaTime;
         if (updateCountdown <= 0)
         {
