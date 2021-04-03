4a6e0181acc8b98cfad04df6741b53c5df796d38
Aaron Doukas <amyleeslave@gmail.com>
2021-04-03 10:45:31 -0700

    Forking office level, monster now has a visual and makes noise.  Detects when it hits player and player makes a death sound.

--- unity/Assets/Scripts/Chase.cs
+++ unity/Assets/Scripts/Chase.cs
@@ -5,15 +5,18 @@ public class Chase : MonoBehaviour
 {
     public Transform target;
     public float updateSeconds;
+    public AudioClip deathNoise;
 
     private float updateCountdown;
     private NavMeshAgent agent;
+    private AudioSource audioSource;
     private bool isPartial;
 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
         agent.destination = target.position;
         updateCountdown = updateSeconds;
         isPartial = false;
@@ -38,6 +41,19 @@ public class Chase : MonoBehaviour
         {
             agent.destination = target.position;
             updateCountdown = updateSeconds;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            audioSource.Stop();
+            audioSource.clip = deathNoise;
+            audioSource.loop = false;
+            audioSource.Play();
+
+            // TODO: wire in other end of game state
         }
     }
 }
