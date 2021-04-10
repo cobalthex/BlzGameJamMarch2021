af5315d918047a450659b48bf1ed32ab8c0a53c8
Aaron Doukas <amyleeslave@gmail.com>
2021-04-10 10:45:51 -0700

    Gun goes pew pew

--- unity/Assets/Prefabs/Pickups/TestGun/Gun.cs
+++ unity/Assets/Prefabs/Pickups/TestGun/Gun.cs
@@ -4,6 +4,7 @@ public class Gun : Equippable
 {
     public Transform Muzzle;
     public Rigidbody Projectile;
+
     public int StartingAmmo = 0;
 
     public float ShotDelaySec = 0.1f;
@@ -13,9 +14,12 @@ public class Gun : Equippable
 
     int ammo = 0;
 
+    private AudioSource audioSource;
+
     private void Start()
     {
         ammo = StartingAmmo;
+        audioSource = GetComponent<AudioSource>();
     }
 
     public override void Use(PlayerController player)
@@ -28,6 +32,8 @@ public class Gun : Equippable
             var shot = Instantiate(Projectile, Muzzle.position + Muzzle.forward * 0.1f, Muzzle.rotation);
             shot.velocity = shot.transform.forward * ShotSpeed;
             --ammo;
+
+            audioSource.Play();
 
             if (StartingAmmo > 0)
                 HintText = $"{ammo} / {StartingAmmo}";
